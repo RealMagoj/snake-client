b9e65c1657773023df6ca4c8bd5ca7dfa0278d92
Dom Golac <domgolac@gmail.com>
2021-04-30 05:39:09 +0000

    refactor

--- client.js
+++ client.js
@@ -1,13 +1,12 @@
+const { IP, PORT } = require("./constants");
 const net = require("net");
 
-// establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: 'localhost',// IP address here,
-    port: 50541// PORT number here,
+    host: IP,
+    port: PORT
   });
 
-  // interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on('connect', () => {
