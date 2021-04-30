dded531db31c2ab9cf34559561824b389c4a0ae3
Dom Golac <domgolac@gmail.com>
2021-04-30 05:18:11 +0000

    update handleUserInput function

--- input.js
+++ input.js
@@ -1,11 +1,28 @@
+let connection;
+
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+  switch (key) {
+    case '\u001b[A':
+      connection.write('Move: up');
+      break;
+    case '\u001b[B':
+      connection.write('Move: down');
+      break;
+    case '\u001b[C':
+      connection.write('Move: right');
+      break;
+    case '\u001b[D':
+      connection.write('Move: left');
+      break;
+  }
 };
 
-const setupInput = function () {
+const setupInput = function (conn) {
   const stdin = process.stdin;
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
